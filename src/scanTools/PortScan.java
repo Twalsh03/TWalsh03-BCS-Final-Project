e5d1f753895b95fd0030332c3d9a575921e2947d
twalsh03 <tomspwalsh@gmail.com>
2021-04-19 17:48:03 +0100

    Port Scan functionality added - configured Netscan to added scan Limit

--- src/scanTools/PortScan.java
+++ src/scanTools/PortScan.java
@@ -5,36 +5,37 @@ import device.Device;
 import java.io.IOException;
 import java.net.*;
 
-public class PortScan{
+public class PortScan {
 
     private int scanLimit;
 
-    public void scan(Device device, String scanType){
+    public void scan(Device device, String scanType) {
 
-        if(scanType.equals("quick")){
-            scanLimit=1024;
-        }else if(scanType.equals("full")){
-            scanLimit=65325;
-        }else{
-            scanLimit=-1;
+        if (scanType.equals("quick")) {
+            scanLimit = 1024;
+        } else if (scanType.equals("full")) {
+            scanLimit = 65325;
+        } else {
+            scanLimit = -1;
         }
 
 
-        for(int port =2; port< scanLimit;port++){
-            Socket socket  = new Socket();
+        for (int port = 2; port < scanLimit; port++) {
+            Socket socket = new Socket();
             try {
                 socket.connect(new InetSocketAddress(device.getIp(), port), 300);
                 socket.close();
                 device.setPort(port);
-            } catch (SocketTimeoutException e){
-                    e.printStackTrace();
-            } catch (BindException e){
+            } catch (SocketTimeoutException e) {
                 e.printStackTrace();
-        } catch (IOException e) {
+            } catch (BindException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             System.out.println(device);
 
 
+        }
     }
 }
