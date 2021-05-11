b89e646935acca0b109f2be94d217056df011f88
Thomas Walsh <twalsh03@student.bbk.ac.uk>
2021-05-11 17:41:13 +0100

    changed tests

--- tests/PortScanTest.java
+++ tests/PortScanTest.java
@@ -6,28 +6,31 @@ import scanTools.PortScan;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PortScanTest {
 
     NetScan testIPScan = new NetScan();
     PortScan portScan = new PortScan();
+    Device routerTEST;
     @Test
     public void  A_OpenPortScanTCPQuickTest() {
         testIPScan = new NetScan();
         portScan = new PortScan();
         testIPScan.scan();
-        Device routerTEST = null;
+
         try {
             routerTEST = new Device(InetAddress.getByName("192.168.8.1"), "94:37:F7:E4:5C:24", "homerouter.cpe");
+            routerTEST.setPort(443, "https");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        routerTEST.setPort(80, "https");
+
         portScan.scanTCP(routerTEST, "quick");
-        boolean actual = routerTEST.getPorts().containsKey(80);
-        assertTrue(actual);
+        Device foundDevice = testIPScan.getFoundDevice(0);
+        boolean expected = routerTEST.getPorts().containsKey(443);
+        boolean actual = foundDevice.getPorts().containsKey(443);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -51,9 +54,8 @@ public class PortScanTest {
         Device TestDevice = testIPScan.getFoundDevice(0);
         portScan.scanUDP(TestDevice, "quick");
         System.out.println(TestDevice);
-        boolean expected = false;
         boolean actual = TestDevice.getPorts().isEmpty();
-        assertEquals(expected, actual);
+        assertFalse(actual);
     }
 }
 
